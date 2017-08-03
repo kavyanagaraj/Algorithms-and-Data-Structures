0e72d8ede027718fa52f4c53ba84074605f9036f
kavyan <knkavyanagaraj@gmail.com>
2017-08-03 00:55:35 -0700

    Adding anagram and anagramCounter

---
+++ anagram.js
@@ -0,0 +1,35 @@
+//Coding challenge
+//Input : ["dog", "god", "cat", "act", "money"]
+//Output : {"dog" : 2, "cat": 2, "money": 1}
+
+function isAnagram (str1, dictAna) {    
+    for(key in dictAna){
+        if (str1.length !== key.length) {
+            continue;
+        }        
+        var sortStr1 = str1.split('').sort().join('');
+        var sortStr2 = key.split('').sort().join('');
+        if(sortStr1 === sortStr2){
+            return key;
+        }
+    }
+    return false;
+}
+
+function anagramCounter(arr){
+    var output = {}
+    for(var i = 0; i < arr.length; i++){
+        var isAna = isAnagram(arr[i], output);
+        if(!isAna){
+            output[arr[i]] = 1;
+        }
+        else{
+            output[isAna] += 1;
+        }
+    }
+    return output;
+}
+
+var anagramArray = ["dog", "god", "cat", "act", "money"]
+var anaCounter = anagramCounter(anagramArray);
+console.log(anaCounter);
