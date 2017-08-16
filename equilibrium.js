653ce2dd5226e6bc978990d3d522d666ff479395
kavyan <knkavyanagaraj@gmail.com>
2017-08-15 23:13:24 -0700

    Finding equilibrium index in an array

---
+++ equilibrium.js
@@ -0,0 +1,21 @@
+// A zero-indexed array A consisting of N integers is given.
+// An equilibrium index of this array is any integer P such that 0 ≤ P < N and the sum of elements
+// of lower indices is equal to the sum of elements of higher indices
+
+function equi(arr){
+  if(arr.length == 0){
+    return -1;
+  }
+  var leftSum = 0;
+  var totalSum = arr.reduce((sum, curr) => {return sum+curr});
+  for(var i = 0; i < arr.length; i++){
+      totalSum -= arr[i];
+      if(leftSum == totalSum){
+        return i;
+      }
+      leftSum += arr[i];
+  }
+}
+
+var eq = equi([-1, 3, -4, 5, 1, -6, 2, 1]);
+console.log(eq);
