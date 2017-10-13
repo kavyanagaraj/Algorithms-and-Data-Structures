74ae58567cff746cc7873100e5ad705c28e567a1
kavyan <knkavyanagaraj@gmail.com>
2017-10-12 17:06:35 -0700

    Adding extract min from a min heap

--- binary_heap.js
+++ binary_heap.js
@@ -8,7 +8,7 @@ class MinHeap{
     }
 }
 
-// Min Heap insert
+// Min Heap insert - logn runtime 
 MinHeap.prototype.insert = function(val){
     this.heap_list.push(val);
     var ind = this.heap_list.length-1;
@@ -25,6 +25,32 @@ MinHeap.prototype.insert = function(val){
     }
 }
 
+MinHeap.prototype.find_min = function(){
+    return this.heap_list[1];
+}
+
+MinHeap.prototype.extract_min = function(){
+    var parent = 1;
+    var min = this.heap_list[parent];
+    var child, child1, min_child;
+    this.heap_list[parent] = this.heap_list[this.heap_list.length-1];
+    this.heap_list.pop();
+    while(this.heap_list[parent] && parent < this.heap_list.length-1){
+        child = parent * 2;
+        child1 = (parent * 2)+1;
+        this.heap_list[child] > this.heap_list[child1] ? min_child = child1 : min_child = child;
+        if(this.heap_list[parent] > this.heap_list[min_child]){
+            let temp = this.heap_list[parent];
+            this.heap_list[parent] = this.heap_list[min_child];
+            this.heap_list[min_child] = temp;
+            parent = min_child;
+        }else{
+            break;
+        }
+    }
+    return min;
+}
+
 var arr = [1,6,4,3,2,0,20,5,8];
 var heap = new MinHeap();
 for(var i = 0; i < arr.length; i++){
@@ -32,4 +58,8 @@ for(var i = 0; i < arr.length; i++){
 }
 
 console.log(heap.heap_list);
+console.log("Minimum element of the heap", heap.find_min());
+console.log("Extract min", heap.extract_min());
+console.log(heap.heap_list);
+
 
