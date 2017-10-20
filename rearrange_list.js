17d0aea8be569bd9e619a11dd64c9ef820efd6f6
kavyan <knkavyanagaraj@gmail.com>
2017-10-19 21:43:36 -0700

    Condensing the loop

--- rearrange_list.js
+++ rearrange_list.js
@@ -7,7 +7,7 @@ function rearrange(arr){
     var j = arr.length-1
     for(var i = 0; i < j; i++){
         if(arr[i] === -1){
-            while(j > 0 && j > i && arr[j] === -1){
+            while(j > i && arr[j] === -1){
                 j--;
             }
             let temp = arr[i];
@@ -17,6 +17,14 @@ function rearrange(arr){
     }
     return arr;
 }
+
+// for (let i = 0; i < j; i++) {
+//     while (arr[i] === -1 && j > i) {
+//       // if arr[j] is not zero, swap arr[i] arr[j], otherwise j--
+//       console.log(`swapping ${i} ${j}`);
+//       arr[j] !== -1? [arr[i], arr[j]] = [arr[j], arr[i]]: j--;
+//     }
+// }
 
 var array = [-1,2,3,5,-1,-1,-1,-1,7,3]
 console.log(rearrange(array));
